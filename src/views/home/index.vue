ec593ea0b3bb24e005fec4669aa4f40da679210f
Cxpller <3273808367@qq.com>
2023-04-04 17:34:32 +0800

    style: 伪元素修改

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,12 +4,13 @@
             :style="{ transform: onSearch ? 'scale(1.2)' : '', filter: onSearch ? 'blur(4px)' : '' }"
             src="@img/VRChat_1.png" alt="" draggable="false">
         <div class="search"
-            :style="{ top: onSearch ? '45%' : '20%', width: onSearch ? '350px' : '280px', backgroundColor: onSearch ? 'rgba(255, 255, 255, 0.25)' : '' }">
+            :style="{ top: onSearch ? '45%' : '20%', width: onSearch ? '350px' : '280px', height: onSearch ? '60px' : '50px', backgroundColor: onSearch ? 'rgba(255, 255, 255, 0.6)' : '' }">
             <div class="search_icon">
                 <img src="@img/home/google.svg" alt="" draggable="false">
             </div>
-            <el-input class="search_input" v-model="search_input" clearable :placeholder="onSearch ? '请输入搜索内容' : '点击搜索'"
-                :style="{ placeholder: onSearch ? 'color:#fff' : '' }" @focus="focusSearch()" @blur="blurSearch()" />
+            <el-input :class="['search_input', onSearch ? 'placeholderClass' : '']" v-model="search_input" clearable
+                :placeholder="onSearch ? '请输入搜索内容' : '点击搜索'" :style="{ placeholder: onSearch ? 'color:#fff' : '' }"
+                @focus="focusSearch()" @blur="blurSearch()" />
             <div class="search_to">
 
             </div>
@@ -53,7 +54,6 @@ const blurSearch = () => {
         position: absolute;
         left: 50%;
         transform: translate(-50%, -50%);
-        height: 50px;
         border-radius: 10px;
         box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         border: 1px solid #f6f6f6;
@@ -62,7 +62,7 @@ const blurSearch = () => {
         transition: 0.6s;
 
         .search_icon {
-            width: 50px;
+            width: 55px;
             height: 100%;
             border-right: 1px solid #f6f6f6;
             display: flex;
@@ -72,11 +72,15 @@ const blurSearch = () => {
             img {
                 width: 24px;
                 height: 24px;
+                transform: translateX(1px)
             }
 
         }
 
         .search_input {
+            border-top-right-radius: 10px;
+            border-bottom-right-radius: 10px;
+
             &:deep(.el-input__wrapper) {
                 background-color: unset;
                 box-shadow: unset;
@@ -84,6 +88,13 @@ const blurSearch = () => {
 
             &:deep(.el-input__inner) {
                 color: #f9f9f9;
+            }
+        }
+
+        .placeholderClass {
+            ::placeholder {
+                transition: 0.6s;
+                color: #fff;
             }
         }
 
